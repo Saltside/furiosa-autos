6d6bb6120e4689c860edf5abc62b7305ca862f02
Himanshu Hiranandani <hhiranandani@deloitte.com>
2016-03-13 13:35:22 +0530

    refactoring models

--- routes/index.js
+++ routes/index.js
@@ -12,17 +12,23 @@ router.get('/', function(req, res, next) {
 * Task 1:
 * Make models alphabetically sortable (ascending, descending, default)
 */
-//TODO find a wat to combine these rules
+var renderModels = function(req, res, next) {
+	return Promise.resolve(api.fetchModels())
+	.then(function(){
+		var sortable = req.params.sortable || 'default';
+		var models = api.fetchModels()
+
+		res.render('models', {models: models._result, sort:sortable});
+	});
+}
+
 router.get('/models/', function(req, res, next) {
-	// use api to get models and render output
-	var models = api.fetchModels()
-	res.render('models', {models: models._result, sort:'default'});
+	renderModels(req, res, next);
 });
 
 router.get('/models/:sortable', function(req, res, next) {
 	// use api to get models and render output
-	var models = api.fetchModels()
-	res.render('models', {models: models._result, sort:req.params.sortable});
+	renderModels(req, res, next);
 });
 
 /*
